04c14519c6cb521e9c8331ab719dedf25fe06f80
Rupesh Bhurke <rupeshbhurke@yahoo.com>
2020-03-10 18:10:38 +0530

    Code from Tutorial two! https://www.youtube.com/watch?v=00QdlHuKGH8 Worked on Drawing Sudoku Board into 9x9 grid. Also added ability for a cell to be selected by User.

--- app/src/main/java/com/rushadvisualexpressions/sudokuyoutube/SudokuBoardView.kt
+++ app/src/main/java/com/rushadvisualexpressions/sudokuyoutube/SudokuBoardView.kt
@@ -5,18 +5,107 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class SudokuBoardView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet){
 
+    private var sqrtSize = 3 // Size of group of cells, 3x3
+    private var size = 9 // Number of rows & Columns in sudoku grid
+    private var cellSizePixels = 0F //Side of an individual cell
+    private var selectedRow = 0 // Current selected row
+    private var selectedCol = 0 // Current selected column
+
     private val thickLinePaint = Paint().apply {
         style = Paint.Style.STROKE
         color = Color.BLACK
-        strokeWidth = 4F;
+        strokeWidth = 6F;
+    } // Group border line
+
+    private val thinLinePaint = Paint().apply {
+        style = Paint.Style.STROKE
+        color = Color.BLACK
+        strokeWidth = 2F;
+    } // Cell border line
+
+    private val selectedCellPaint = Paint().apply {
+        style = Paint.Style.FILL_AND_STROKE
+        color = Color.parseColor("#C8C8C8")
+    }
+
+    private val conflictingCellPaint = Paint().apply {
+        style = Paint.Style.FILL_AND_STROKE
+        color = Color.parseColor("#DADADA")
+    }
+
+    //
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val sizeInPixels = Math.min(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(sizeInPixels, sizeInPixels);
     }
 
     override fun onDraw(canvas: Canvas) {
-        //super.onDraw(canvas)
+        cellSizePixels = (width / size).toFloat()
+        fillCells(canvas)
+        drawLines(canvas)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                handleTouchEvent(event.x, event.y)
+                true;
+            }
+            else->  false
+        }
+    }
+
+    private fun handleTouchEvent(x: Float, y: Float) {
+        selectedRow = (y / cellSizePixels).toInt()
+        selectedCol = (x / cellSizePixels).toInt()
+        invalidate()
+    }
+
+    private fun fillCells(canvas: Canvas) {
+        if ( selectedRow == -1 || selectedCol == -1) return
+        for (r in 0..size) {
+            for (c in 0..size) {
+                if (r == selectedRow && c == selectedCol) {
+                    fillCell(canvas, r, c, selectedCellPaint)
+                } else if (r == selectedRow || c == selectedCol) {
+                    fillCell(canvas, r, c, conflictingCellPaint)
+                } else if (r / sqrtSize == selectedRow / sqrtSize && c / sqrtSize == selectedCol / sqrtSize) {
+                    fillCell(canvas, r, c, conflictingCellPaint)
+                }
+            }
+        }
+    }
+
+    private fun fillCell(canvas: Canvas, r: Int, c: Int, paint: Paint) {
+        canvas.drawRect( c * cellSizePixels, r * cellSizePixels, (c + 1) * cellSizePixels, (r+1) * cellSizePixels, paint);
+    }
+
+    private fun drawLines(canvas: Canvas) {
         canvas.drawRect(0F, 0F, width.toFloat(), height.toFloat(), thickLinePaint)
+        for (i in 1 until size) {
+            val paintToUse = when( i % sqrtSize ) {
+                0 -> thickLinePaint
+                else -> thinLinePaint
+            }
+            canvas.drawLine(
+                i*cellSizePixels,
+                0F,
+                i*cellSizePixels,
+                height.toFloat(),
+                paintToUse)
+            canvas.drawLine(
+                0F,
+                i*cellSizePixels,
+                width.toFloat(),
+                i*cellSizePixels,
+                paintToUse
+            );
+        }
     }
 }
